433d1a38fd6528a8e837d2206e3aa4047dabbdea
prasantasonalkunal-ui <prasantasonalkunal@gmail.com>
2026-06-23 04:16:48 +0000

    Copied content from maven1 folder into maven2 branch

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,31 +1,19 @@
 package com.mycompany.app;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
 public class AppTest {
 
+    /**
+     * Rigorous Test :-)
+     */
     @Test
-    public void testAdd() {
-        App app = new App();
-        assertEquals(5, app.add(2, 3));
-    }
-
-    @Test
-    public void testAddWithZero() {
-        App app = new App();
-        assertEquals(5, app.add(5, 0));
-    }
-
-    @Test
-    public void testSubtract() {
-        App app = new App();
-        assertEquals(2, app.subtract(5, 3));
-    }
-
-    @Test
-    public void testSubtractNegative() {
-        App app = new App();
-        assertEquals(-1, app.subtract(2, 3));
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
